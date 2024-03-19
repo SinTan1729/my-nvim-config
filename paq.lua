032a6cd807f155ced89c8d0caed0519d65466c19
SinTan1729 <sayantan.santra689@gmail.com>
2024-03-19 03:59:14 -0500

    fix: Tabbing in paq.lua

--- paq.lua
+++ paq.lua
@@ -3,21 +3,21 @@ local function clone_paq()
     local path = vim.fn.stdpath("data") .. "/site/pack/paqs/start/paq-nvim"
     local is_installed = vim.fn.empty(vim.fn.glob(path)) == 0
     if not is_installed then
-      vim.fn.system { "git", "clone", "--depth=1", "https://github.com/savq/paq-nvim.git", path }
-      return true
+        vim.fn.system { "git", "clone", "--depth=1", "https://github.com/savq/paq-nvim.git", path }
+        return true
     end
-  end
+end
 
-  local function bootstrap_paq(packages)
+local function bootstrap_paq(packages)
     local first_install = clone_paq()
     vim.cmd.packadd("paq-nvim")
     local paq = require("paq")
     if first_install then
-      vim.notify("Installing plugins... If prompted, hit Enter to continue.")
-      paq.install()
+        vim.notify("Installing plugins... If prompted, hit Enter to continue.")
+        paq.install()
     end
     paq(packages)
-  end
+end
 
 -- Automatically install new packages at startup
 vim.api.nvim_create_autocmd("VimEnter", {
@@ -35,42 +35,42 @@ vim.api.nvim_create_autocmd("VimEnter", {
 
 -- Load plugin via paq-nvim
 bootstrap_paq {
-  "savq/paq-nvim",
-  -- airline related plugins
-  "vim-airline/vim-airline",
-  "vim-airline/vim-airline-themes",
-  -- Auto commenting per filetype
-  "numToStr/Comment.nvim",
-  -- Give option to save files using sudo, if needed
-  "lambdalisue/suda.vim",
-  -- Auto toggle for number mode when vim isn't focused
-  "sitiom/nvim-numbertoggle",
-  -- Plugin for lean
-  "julian/lean.nvim",
-  -- LSP related plugins
-  "neovim/nvim-lspconfig",
-  "nvim-lua/plenary.nvim",
-  "hrsh7th/nvim-cmp",        -- For LSP completion
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-omni",        -- For LaTeX completion
-  "hrsh7th/cmp-path",
-  "hrsh7th/cmp-cmdline",
-  "SirVer/ultisnips",        -- For snippets
-  -- Support programming terms
-  { "psliwka/vim-dirtytalk", build = ':let &rtp = &rtp | DirtytalkUpdate' },
-  -- vim-moonfly theme
-  { "bluz71/vim-moonfly-colors", as = 'moonfly' },
-  -- Rust tools
-  "simrat39/rust-tools.nvim",
-  -- Automatically add bracket pairs
-  "windwp/nvim-autopairs",
-  -- Syntax highlighting for Fish scripts
-  "khaveesh/vim-fish-syntax",
-  -- Plugin for LaTeX
-  "lervag/vimtex",
-  -- Formatter
-  "stevearc/conform.nvim",
-  -- For Searching
-  "junegunn/fzf.vim",
+    "savq/paq-nvim",
+    -- airline related plugins
+    "vim-airline/vim-airline",
+    "vim-airline/vim-airline-themes",
+    -- Auto commenting per filetype
+    "numToStr/Comment.nvim",
+    -- Give option to save files using sudo, if needed
+    "lambdalisue/suda.vim",
+    -- Auto toggle for number mode when vim isn't focused
+    "sitiom/nvim-numbertoggle",
+    -- Plugin for lean
+    "julian/lean.nvim",
+    -- LSP related plugins
+    "neovim/nvim-lspconfig",
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",        -- For LSP completion
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-omni",        -- For LaTeX completion
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "SirVer/ultisnips",        -- For snippets
+    -- Support programming terms
+    { "psliwka/vim-dirtytalk", build = ':let &rtp = &rtp | DirtytalkUpdate' },
+    -- vim-moonfly theme
+    { "bluz71/vim-moonfly-colors", as = 'moonfly' },
+    -- Rust tools
+    "simrat39/rust-tools.nvim",
+    -- Automatically add bracket pairs
+    "windwp/nvim-autopairs",
+    -- Syntax highlighting for Fish scripts
+    "khaveesh/vim-fish-syntax",
+    -- Plugin for LaTeX
+    "lervag/vimtex",
+    -- Formatter
+    "stevearc/conform.nvim",
+    -- For Searching
+    "junegunn/fzf.vim",
 }
