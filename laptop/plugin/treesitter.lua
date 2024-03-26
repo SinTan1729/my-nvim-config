a2748ac0e9985f102ec1a8835a9d527747abe7f6
SinTan1729 <sayantan.santra689@gmail.com>
2024-03-26 01:40:20 -0500

    chg: Added more languages to treesitter

--- laptop/plugin/treesitter.lua
+++ laptop/plugin/treesitter.lua
@@ -1,7 +1,8 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup({
-    ensure_installed = { "rust", "lua", "vim", "vimdoc", "latex", "ocaml", "yaml", "dockerfile", "make", "bash" },
+    ensure_installed = { "rust", "lua", "vim", "vimdoc", "latex", "ocaml",
+        "yaml", "dockerfile", "make", "bash", "html", "javascript", "css" },
     sync_install = false,
     highlight = { enable = true },
     indent = { enable = true },
