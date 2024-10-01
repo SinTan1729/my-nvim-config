2ce8782d14778a834585c74a9a382beb167c6960
SinTan1729 <sayantan.santra689@gmail.com>
2024-10-01 12:27:25 -0500

    new: Use .caddy extension for Caddyfiles

--- vps/config/globals.lua
+++ vps/config/globals.lua
@@ -28,3 +28,10 @@ g.loaded_perl_provider = 0
 g.loaded_node_provider = 0
 g.loaded_ruby_provider = 0
 
+-- Custom filetypes
+vim.filetype.add({
+  pattern = {
+    ['.*/.*.caddy'] = 'caddyfile',
+  },
+})
+
