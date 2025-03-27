d987981adb154c3fb460a2e0f622267ea32ab83e
SinTan1729 <sayantan.santra689@gmail.com>
2025-03-27 01:39:44 -0500

    new: Added new LSP related keymaps

--- laptop/plugin/lsp_config.lua
+++ laptop/plugin/lsp_config.lua
@@ -22,12 +22,18 @@ lspconfig.ocamllsp.setup {}
 
 lspconfig.hls.setup {}
 
--- Set ;k to hover and ;a to show code actions
+-- ;k to hover
+-- ;a to show code actions
+-- ;d to show diagnostic message
+-- ;n to go to next diagnostic message
+-- ;N to go to previous diagnostic message
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function()
         map('n', '<localleader>k', vim.lsp.buf.hover, { remap = false })
         map('n', '<localleader>a', vim.lsp.buf.code_action, { remap = false })
         map('n', '<localleader>d', vim.diagnostic.open_float, { remap = false })
+        map('n', '<localleader>n', vim.diagnostic.goto_next, { remap = false })
+        map('n', '<localleader>N', vim.diagnostic.goto_prev, { remap = false })
     end,
 })
 
