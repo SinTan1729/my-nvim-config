7bd4a143a09a96ebfc330dc27bdeec4504214ccd
SinTan1729 <sayantan.santra689@gmail.com>
2025-05-09 00:37:21 -0500

    fix: No border on hover

--- laptop/plugin/moonfly.lua
+++ laptop/plugin/moonfly.lua
@@ -1,19 +1,8 @@
 -- Use moonfly colors in popups
 vim.g.moonflyNormalFloat = true
 
--- Extra setup to distinguish between edit and floating windows
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-    vim.lsp.handlers.hover, {
-        border = "single"
-    }
-)
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-    vim.lsp.handlers.signatureHelp, {
-        border = "single"
-    }
-)
-vim.diagnostic.config({ float = { border = "single" } })
--- Some more setup is inside cmp.lua
+-- Distinguish between the edit and floating windows
+vim.o.winborder = "single"
 
 -- Make the background transparent
 vim.g.moonflyTransparent = true
@@ -23,3 +12,5 @@ vim.g.moonflyVirtualTextColor = true
 
 --  Use the moonfly colorscheme
 vim.cmd [[colorscheme moonfly]]
+
+-- Some more setup is inside cmp.lua
