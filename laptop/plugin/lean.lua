82006d463efc70d5c37bc4de03bce8ea8927c9dd
SinTan1729 <sayantan.santra689@gmail.com>
2026-02-08 20:19:28 -0600

    fix: Lean LSP configs

--- laptop/plugin/lean.lua
+++ laptop/plugin/lean.lua
@@ -1,32 +1,15 @@
--- You may want to reference the nvim-lspconfig documentation, found at:
--- https://github.com/neovim/nvim-lspconfig#keybindings-and-completion
--- The below is just a simple initial set of mappings which will be bound
--- within Lean files.
 local function on_attach(_, bufnr)
-    local function cmd(mode, lhs, rhs)
-        vim.keymap.set(mode, lhs, rhs, { noremap = true, buffer = true })
-    end
-
     -- Autocomplete using the Lean language server
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- gd in normal mode will jump to definition
-    cmd('n', 'gd', vim.lsp.buf.definition)
-
-    -- <localleader>q will load all errors in the current lean file into the location list
-    -- (and then will open the location list)
-    -- see :h location-list if you don't generally use it in other vim contexts
-    cmd('n', '<localleader>q', vim.diagnostic.setloclist)
 end
 
 -- Enable lean.nvim, and enable abbreviations and mappings
-vim.lsp.config('leanls', {
-    abbreviations = { builtin = true },
-    lsp = { on_attach = on_attach },
-    lsp3 = { on_attach = on_attach },
+require('lean').setup({
     mappings = true,
 })
-vim.lsp.enable('leanls')
+vim.lsp.config('leanls', {
+    on_attach = on_attach,
+})
 
 -- Enable virtual text for Lean
 vim.api.nvim_create_autocmd("LspAttach", {
