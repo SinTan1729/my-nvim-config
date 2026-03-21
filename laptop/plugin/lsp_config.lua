060fe65b8a3c3d3917076faa84ce19175c939f85
SinTan1729 <sayantan.santra689@gmail.com>
2026-03-21 11:56:30 -0500

    chg: Shift to ty from pyright

--- laptop/plugin/lsp_config.lua
+++ laptop/plugin/lsp_config.lua
@@ -21,59 +21,13 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 
 -- Python
+vim.lsp.enable('ty')
 lsp_config('ruff', {
     init_options = {
         settings = {
             lineLength = 100,
         }
     }
-})
-
-local function get_python_path(workspace)
-    if vim.env.VIRTUAL_ENV then
-        return vim.env.VIRTUAL_ENV .. '/bin/python'
-    end
-
-    workspace = workspace or vim.fn.getcwd()
-    local pycfg = io.open(workspace .. '/.venv/pyvenv.cfg', "r")
-    if pycfg then
-        pycfg:close()
-        return workspace .. '/.venv/bin/python'
-    end
-
-    return 'python'
-end
-lsp_config('pyright', {
-    before_init = function(_, config)
-        config.settings.python.pythonPath = get_python_path(config.root_dir)
-    end,
-    settings = {
-        pyright = {
-            -- Using Ruff's import organizer
-            disableOrganizeImports = true,
-        },
-        python = {
-            analysis = {
-                -- Ignore all files for analysis to exclusively use Ruff for linting
-                ignore = { '*' },
-            },
-        },
-    },
-})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup('lsp_attach_python', { clear = true }),
-    callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if client == nil then
-            return
-        end
-        if client.name == 'ruff' then
-            -- Disable hover in favor of Pyright
-            client.server_capabilities.hoverProvider = false
-        end
-    end,
-    desc = 'LSP: Disable hover capability from Ruff',
 })
 
 -- Lua
