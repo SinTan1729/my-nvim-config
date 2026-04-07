c8408cb5dc1f15b0a4485a803310317d88da7094
SinTan1729 <sayantan.santra689@gmail.com>
2026-04-06 23:40:08 -0500

    new: We can safely commit all files here

---
+++ push-pull.sh
@@ -0,0 +1,22 @@
+DIR="$HOME/Code/git/my-nvim-config"
+
+if [ "$1" = "push" ]; then
+    echo "Sending to laptop..."
+    rsync -aSXHhP --exclude={'*.md','*.sh','LICENSE','.git/'} "$DIR/" "$HOME/.config/nvim/" --delete-excluded
+    for server in server vps pi3b pizero; do
+        if ping -qc2 -W5 $server-ts >/dev/null; then
+            echo "Sending to $server..."
+            rsync -aSXHhP --exclude={'*.md','*.sh','LICENSE','.git/','snips/','spell/'} "$DIR/" $server-ts-rsync:"~/.config/nvim/" --delete-excluded
+        else
+            echo "Could not connect to $server!"
+        fi
+    done
+elif [ "$1" = "pull" ]; then
+    echo "Pulling from laptop..."
+    rsync -aSXHhP --delete \
+        --filter='P *.sh' --filter='P *.md' \
+        --filter='P LICENSE' --filter='P .*' \
+        "$HOME/.config/nvim/" "$DIR/"
+else
+    echo "Please supply push/pull."
+fi
