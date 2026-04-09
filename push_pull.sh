5538d4de964924ff7abcde3e8fa468072f20a7b3
SinTan1729 <sayantan.santra689@gmail.com>
2026-04-09 12:34:39 -0500

    chg: laptop-push -> push-local in push pull script

--- push_pull.sh
+++ push_pull.sh
@@ -5,6 +5,7 @@ DIR="$HOME/Code/git/my-nvim-config"
 set -e
 
 if [ "$1" = "push" ]; then
+    echo "Sending to laptop..."
     cd ~/.config/nvim
     git fetch origin
     git reset --hard origin/private
@@ -25,9 +26,9 @@ EOF
             echo "Could not connect to $server!"
         fi
     done
-elif [ "$1" = "laptop-push" ]; then
-    echo "Sending to laptop..."
-    rsync -achP --exclude={'*.md','*.sh','LICENSE','.*'} "$DIR/" "$HOME/.config/nvim/" --delete-excluded
+elif [ "$1" = "push-local" ]; then
+    echo "Sending to laptop locally..."
+    rsync -achP --exclude={'*.md','*.sh','LICENSE','.*'} "$DIR/" "$HOME/.config/nvim/" --delete
 elif [ "$1" = "pull" ]; then
     echo "Pulling from laptop..."
     rsync -achP --delete \
