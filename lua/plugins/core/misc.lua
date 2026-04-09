61d8abf5ca01a5d07f0b6ff3535a1c8c8c601210
SinTan1729 <sayantan.santra689@gmail.com>
2026-04-09 12:34:01 -0500

    new: Enabled snacks notify

--- lua/plugins/core/misc.lua
+++ lua/plugins/core/misc.lua
@@ -3,4 +3,17 @@ return {
     { 'sairyy/zshow.nvim',       lazy = false },
     { 'sitiom/nvim-numbertoggle' },
     { 'kylechui/nvim-surround',  opts = {} },
+    {
+        'folke/snacks.nvim',
+        priority = 1000,
+        lazy = false,
+        opts = {
+            notifier = { enabled = true },
+        },
+        config = function(_, opts)
+            require("snacks").setup(opts)
+            -- Override default notify
+            vim.notify = require("snacks.notifier").notify
+        end,
+    },
 }
