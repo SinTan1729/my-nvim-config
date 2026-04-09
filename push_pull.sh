164bf1e3507dfaa126dd10c7e577e46cdd117da6
SinTan1729 <sayantan.santra689@gmail.com>
2026-04-09 10:23:41 -0500

    chg: Improved push_pull.sh to use git

--- push_pull.sh
+++ push_pull.sh
@@ -5,17 +5,29 @@ DIR="$HOME/Code/git/my-nvim-config"
 set -e
 
 if [ "$1" = "push" ]; then
-    echo "Sending to laptop..."
-    rsync -achP --exclude={'*.md','*.sh','LICENSE','.*'} "$DIR/" "$HOME/.config/nvim/" --delete-excluded
-    [ "$2" = "laptop-only" ] && exit
+    cd ~/.config/nvim
+    git fetch origin
+    git reset --hard origin/private
+    git clean -fd
+    rm -f *.sh *md LICENSE
     for server in server vps pi3b pizero; do
         if ping -qc2 -W5 $server-ts >/dev/null; then
             echo "Sending to $server..."
-            rsync -achP --exclude={'*.md','*.sh','LICENSE','.*','snips/','spell/'} "$DIR/" $server-ts-rsync:"~/.config/nvim/" --delete-excluded
+            ssh $server-ts-plain -T /bin/bash <<'EOF'
+                cd ~/.config/nvim
+                git fetch origin
+                git reset --hard origin/main
+                git clean -fd
+                rm -rf snips
+                rm -f *md LICENSE
+EOF
         else
             echo "Could not connect to $server!"
         fi
     done
+elif [ "$1" = "laptop-push" ]; then
+    echo "Sending to laptop..."
+    rsync -achP --exclude={'*.md','*.sh','LICENSE','.*'} "$DIR/" "$HOME/.config/nvim/" --delete-excluded
 elif [ "$1" = "pull" ]; then
     echo "Pulling from laptop..."
     rsync -achP --delete \
