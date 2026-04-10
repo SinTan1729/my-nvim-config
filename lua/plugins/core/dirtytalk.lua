0afb5e15cc0352cf587fa412fe82edd19400207a
SinTan1729 <sayantan.santra689@gmail.com>
2026-04-10 11:35:53 -0500

    chg: Temporarily use patched versions of some plugins unless fixes are merged

--- lua/plugins/core/dirtytalk.lua
+++ lua/plugins/core/dirtytalk.lua
@@ -1,6 +1,7 @@
 return {
-    'psliwka/vim-dirtytalk',
+    'sak96/vim-dirtytalk',
     build = ':DirtytalkUpdate',
+    commit = '88f7423b0627bbe37fb434ef9c71dd7fe41cc5b5',
     init = function()
         vim.opt.spelllang:append('programming')
     end
