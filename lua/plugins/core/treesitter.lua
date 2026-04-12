a813c68bf1f1e7cf7b4babf5d11fc4a183f3f8c9
SinTan1729 <sayantan.santra689@gmail.com>
2026-04-12 15:35:22 -0500

    fix: Normalization of entries

--- lua/plugins/core/treesitter.lua
+++ lua/plugins/core/treesitter.lua
@@ -4,13 +4,15 @@ return {
     lazy = false,
     branch = 'main',
     dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        init = function()
-            vim.g.no_plugin_maps = true
-        end,
-        opts = {
-            select = { lookahead = true },
-            move = { set_jumps = true },
+        {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+            init = function()
+                vim.g.no_plugin_maps = true
+            end,
+            opts = {
+                select = { lookahead = true },
+                move = { set_jumps = true },
+            },
         },
     },
     opts = {
