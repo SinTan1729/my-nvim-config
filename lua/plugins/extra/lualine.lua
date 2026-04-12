66ac45d4373971d12e1a4ec6b328c39b1081cf93
SinTan1729 <sayantan.santra689@gmail.com>
2026-04-12 16:48:56 -0500

    new: Added arrow.nvim for per-project marks

--- lua/plugins/extra/lualine.lua
+++ lua/plugins/extra/lualine.lua
@@ -1,13 +1,29 @@
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'linrongbin16/lsp-progress.nvim' },
+    dependencies = {
+        'linrongbin16/lsp-progress.nvim',
+        'otavioschwanck/arrow.nvim',
+    },
     opts = function(_, opts)
         local lua_progress = function()
             return require('lsp-progress').progress()
         end
 
+        local function arrow_status() -- from lualine documentation
+            return require('arrow.statusline').text_for_statusline_with_icons()
+        end
+
         opts.sections.lualine_c = { 'filename', lua_progress, 'searchcount' }
+        opts.tabline.lualine_y = { arrow_status }
 
         return opts
+    end,
+    config = function(_, opts)
+        require('lualine').setup(opts)
+        vim.api.nvim_create_autocmd('User', {
+            desc = 'Update lualine progress',
+            pattern = 'LspProgressStatusUpdated',
+            callback = require('lualine').refresh,
+        })
     end,
 }
