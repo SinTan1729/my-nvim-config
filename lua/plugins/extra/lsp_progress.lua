446c914aee04ec3565ae594987691c5436d2c4a8
SinTan1729 <sayantan.santra689@gmail.com>
2026-04-13 16:35:13 -0500

    chg: Show the arrow marks in the tab name itself

--- lua/plugins/extra/lsp_progress.lua
+++ lua/plugins/extra/lsp_progress.lua
@@ -2,26 +2,26 @@ return {
     'linrongbin16/lsp-progress.nvim',
     event = 'LspAttach',
     opts = {
-        spinner = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" },
+        spinner = { '⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷' },
 
         client_format = function(client, spinner, series_messages)
             if series_messages and #series_messages > 0 then
                 local name = client and client.name
                     or table.concat(vim.tbl_map(function(c) return c.name end,
-                        vim.lsp.get_active_clients()), ", ")
-                return spinner .. " " .. name
+                        vim.lsp.get_active_clients()), ', ')
+                return spinner .. ' ' .. name
             end
-            return ""
+            return ''
         end,
 
         format = function(messages)
             if messages and #messages > 0 then
-                return table.concat(messages, " ")
+                return table.concat(messages, ' ')
             end
 
             local names = vim.tbl_map(function(c) return c.name end,
                 vim.lsp.get_active_clients())
-            return #names > 0 and (" " .. table.concat(names, ", ")) or ""
+            return #names > 0 and (' ' .. table.concat(names, ', ')) or ''
         end,
     }
 }
