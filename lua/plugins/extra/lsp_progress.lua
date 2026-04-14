940da049d20615ee99a610af3bd74bb4a5bfddd6
SinTan1729 <sayantan.santra689@gmail.com>
2026-04-13 19:36:24 -0500

    chg: Factor out the get_client_names function.

--- lua/plugins/extra/lsp_progress.lua
+++ lua/plugins/extra/lsp_progress.lua
@@ -1,27 +1,33 @@
 return {
     'linrongbin16/lsp-progress.nvim',
     event = 'LspAttach',
-    opts = {
-        spinner = { '⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷' },
+    opts = function()
+        local get_client_names = function()
+            return table.concat(vim.tbl_map(function(c) return c.name end,
+                vim.lsp.get_active_clients()), ', ')
+        end
 
-        client_format = function(client, spinner, series_messages)
-            if series_messages and #series_messages > 0 then
-                local name = client and client.name
-                    or table.concat(vim.tbl_map(function(c) return c.name end,
-                        vim.lsp.get_active_clients()), ', ')
-                return spinner .. ' ' .. name
-            end
-            return ''
-        end,
+        local opts = {
+            spinner = { '⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷' },
 
-        format = function(messages)
-            if messages and #messages > 0 then
-                return table.concat(messages, ' ')
-            end
+            client_format = function(client, spinner, series_messages)
+                if series_messages and #series_messages > 0 then
+                    local names = client and client.name
+                        or get_client_names()
+                    return spinner .. ' ' .. names
+                end
+                return ''
+            end,
 
-            local names = vim.tbl_map(function(c) return c.name end,
-                vim.lsp.get_active_clients())
-            return #names > 0 and (' ' .. table.concat(names, ', ')) or ''
-        end,
-    }
+            format = function(messages)
+                if messages and #messages > 0 then
+                    return table.concat(messages, ' ')
+                end
+
+                local names = get_client_names()
+                return names ~= '' and (' ' .. names) or ''
+            end,
+        }
+        return opts
+    end,
 }
