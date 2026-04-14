c4cf061311ad7a8304f0a6b3a448b2254463a497
SinTan1729 <sayantan.santra689@gmail.com>
2026-04-13 19:29:26 -0500

    new: Added some new TS filetypes

--- lua/plugins/extra/treesitter.lua
+++ lua/plugins/extra/treesitter.lua
@@ -3,7 +3,8 @@ return {
     dependencies = { 'hiphish/rainbow-delimiters.nvim' },
     opts = function(_, opts)
         local extra_filetypes = { 'rust', 'ocaml', 'html', 'javascript', 'css',
-            'go', 'haskell' }
+            'go', 'haskell', 'regex', 'markdown', 'markdown_inline', 'xml', 'c',
+            'cpp' }
         table.move(extra_filetypes, 1, #extra_filetypes, #opts.ts_filetypes + 1, opts.ts_filetypes)
         return opts
     end,
