0ce6bd5ca10bc77edd3c0f31f91ce6fe06a2cbe5
SinTan1729 <sayantan.santra689@gmail.com>
2026-04-24 12:53:16 -0500

    chg: Switch back to official academic.nvim

--- lua/plugins/extra/misc.lua
+++ lua/plugins/extra/misc.lua
@@ -1,10 +1,8 @@
 return {
     -- Some miscellaneous plugins
     {
-        'danielfleischer/academic.nvim',
+        'ficd0/academic.nvim',
         build = ':AcademicBuild',
-        branch = 'patch-1',
-        commit = '48048fed33fa862d12bd61a864721bf1ad5a1ee9',
     },
     { 'fei6409/log-highlight.nvim', opts = {} },
     {
