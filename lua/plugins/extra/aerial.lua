e0ec16ccd3ac0e9d3cb08801507fe7191b160b94
SinTan1729 <sayantan.santra689@gmail.com>
2026-05-13 09:36:49 -0500

    new: Added aerial.nvim

---
+++ lua/plugins/extra/aerial.lua
@@ -0,0 +1,22 @@
+return {
+    'stevearc/aerial.nvim',
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+        'nvim-tree/nvim-web-devicons',
+    },
+    opts = {
+        layout = {
+            min_width = 30,
+            placement = 'edge',
+        },
+        close_automatic_events = { 'unsupported' },
+        on_attach = function(bufnr)
+            vim.keymap.set('n', '<localleader>{', '<cmd>AerialPrev<CR>',
+                { buffer = bufnr, remap = false, desc = 'Jump to previous item on Aerial' })
+            vim.keymap.set('n', '<localleader>}', '<cmd>AerialNext<CR>',
+                { buffer = bufnr, remap = false, desc = 'Jump to next item on Aerial' })
+            vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle!<CR>',
+                { remap = false, desc = 'Toggle Aerial bar' })
+        end,
+    },
+}
