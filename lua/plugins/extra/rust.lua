f6f1b8091c2d7489b8509f33c3f3cd1ec3a8c697
SinTan1729 <sayantan.santra689@gmail.com>
2026-06-29 20:56:34 -0500

    fix: Buggy squiggly lines from rust analyzer

--- lua/plugins/extra/rust.lua
+++ lua/plugins/extra/rust.lua
@@ -8,5 +8,13 @@ return {
                 enable_clippy = true,
             },
         }
+        local group = vim.api.nvim_create_augroup('rust-lsp', { clear = true })
+        vim.api.nvim_create_autocmd('LspAttach', {
+            desc = 'Disable buggy unresolvedReference semantics tokens in rust',
+            group = group,
+            callback = function()
+                vim.api.nvim_set_hl(0, '@lsp.type.unresolvedReference.rust', {})
+            end,
+        })
     end,
 }
